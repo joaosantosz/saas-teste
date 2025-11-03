0be4ff8ea19c7f5adddb2e45f6b3be56c1ed9d9f
joaosantosz <joaogabrielsantosg12@gmail.com>
2025-11-03 13:24:11 -0500

    Update page.js

--- app/page.js
+++ app/page.js
@@ -10,7 +10,7 @@ export default function Home() {
   const [loading, setLoading] = useState(false);
   const [linkDeclaracao, setLinkDeclaracao] = useState("");
 
-  // Função para gerar slug amigável
+  // Gera slug amigável
   function gerarSlug(texto) {
     return texto
       .toLowerCase()
